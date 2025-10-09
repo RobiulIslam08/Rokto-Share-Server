420952c75147f2ec28139a3914c722c665a45d2b
Robiul Islam <robiulislamrobi0874@gmail.com>
2025-10-09 11:40:24 +0600

    catchAsync function create

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -10,6 +10,7 @@ dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 export default {
   port: process.env.PORT,
+   NODE_ENV: process.env.NODE_ENV,
 
   database_url: process.env.DATABASE_URL,
 };
