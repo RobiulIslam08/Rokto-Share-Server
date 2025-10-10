2b9de1d32f65aa91e15029be6ad82f9a4f18b22d
Robiul Islam <robiulislamrobi0874@gmail.com>
2025-10-10 21:12:54 +0600

    create auth interface

--- src/app/modules/Auth/auth.interface.ts
+++ src/app/modules/Auth/auth.interface.ts
@@ -1,1 +1,9 @@
 export type TUserRole = 'user' | 'donor' | 'admin';
+export type TUser = {
+  name: string;
+  email: string;
+  password?: string;
+  phone: string;
+  role: TUserRole;
+  passwordChangedAt?: Date;
+};
