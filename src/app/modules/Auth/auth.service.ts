43deca0799b49ae57bf116019c40bd0b2d5ea4b8
Robiul Islam <robiulislamrobi0874@gmail.com>
2025-10-12 11:53:22 +0600

    login functionality create

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -4,6 +4,7 @@ import { TUser } from './auth.interface';
 import { User } from './auth.model';
 import jwt from 'jsonwebtoken';
 import config from '../../config';
+import { createToken } from './auth.utils';
 const loginUser = async (payload: Pick<TUser, 'email' | 'password'>) => {
   const user = await User.findOne({ email: payload?.email }).select(
     '+password',
@@ -16,7 +17,15 @@ const loginUser = async (payload: Pick<TUser, 'email' | 'password'>) => {
     user.password as string,
   );
   const jwtPayload = {
-    userId: user?._id,
+    userId: user?.id,
     role: user?.role,
   };
+    const accessToken = createToken(jwtPayload,config.jwt_access_secret as string,config.jwt_access_expires_in as string)
+
+	const loggedInUser = await User.findOne({email: payload.email});
+
+    return {
+      accessToken,
+      user: loggedInUser
+    };
 };
