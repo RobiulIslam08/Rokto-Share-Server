b162821a9f51cb38686d2951ec448b7da2f23bf2
Robiul Islam <robiulislamrobi0874@gmail.com>
2025-10-12 12:28:28 +0600

    user validtion create

---
+++ src/app/modules/User/user.model.ts
@@ -0,0 +1,26 @@
+import { Schema, model } from 'mongoose';
+import { TUserProfile } from './user.interface';
+
+const bloodGroups = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'];
+const locationSchema = new Schema({
+  division: { type: String, required: true },
+  district: { type: String, required: true },
+  upazila: { type: String, required: true },
+}, {_id: false});
+
+const userProfileSchema = new Schema<TUserProfile>(
+  {
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true, unique: true },
+    bloodGroup: { type: String, enum: bloodGroups,required: true },
+    age: { type: Number, required: true },
+    weight: { type: Number, required: true },
+    location: { type: locationSchema, required: true },
+    isAvailable: { type: Boolean, default: true },
+    lastDonationDate: { type: String },
+    medicalHistory: { type: String },
+    previousDonations: { type: Number, default: 0 },
+  },
+  { timestamps: true },
+);
+
+export const UserProfile = model<TUserProfile>('UserProfile', userProfileSchema);
