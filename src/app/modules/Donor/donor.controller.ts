c7b5870c1842a9406a54c8ec59ca3b21582269dd
Robiul Islam <robiulislamrobi0874@gmail.com>
2025-10-14 15:09:31 +0600

    get all donor controller create

--- src/app/modules/Donor/donor.controller.ts
+++ src/app/modules/Donor/donor.controller.ts
@@ -1,35 +1,35 @@
-// // src/app/modules/Donor/donor.controller.ts
-// import { Request, Response } from 'express';
-// import catchAsync from '../../utils/catchAsync';
-// import sendResponse from '../../utils/sendResponse';
-// import httpStatus from 'http-status';
-// import { DonorServices } from './donor.service';
+// src/app/modules/Donor/donor.controller.ts
+import { Request, Response } from 'express';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import httpStatus from 'http-status';
+import { DonorServices } from './donor.service';
 
-// const getAllDonors = catchAsync(async (req: Request, res: Response) => {
-//   const result = await DonorServices.getAllDonorsFromDB(req.query);
+const getAllDonors = catchAsync(async (req: Request, res: Response) => {
+  const result = await DonorServices.getAllDonorsFromDB(req.query);
 
-//   sendResponse(res, {
-//     statusCode: httpStatus.OK,
-//     success: true,
-//     message: 'Donors retrieved successfully',
-//     meta: result.meta,
-//     data: result.data,
-//   });
-// });
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Donors retrieved successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
 
-// const getSingleDonor = catchAsync(async (req: Request, res: Response) => {
-//   const { id } = req.params;
-//   const result = await DonorServices.getSingleDonorFromDB(id);
+const getSingleDonor = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await DonorServices.getSingleDonorFromDB(id);
 
-//   sendResponse(res, {
-//     statusCode: httpStatus.OK,
-//     success: true,
-//     message: 'Donor retrieved successfully',
-//     data: result,
-//   });
-// });
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Donor retrieved successfully',
+    data: result,
+  });
+});
 
-// export const DonorControllers = {
-//   getAllDonors,
-//   getSingleDonor,
-// };
+export const DonorControllers = {
+  getAllDonors,
+  getSingleDonor,
+};
